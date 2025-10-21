c49333f3ab96dbe8fe91b4c57ecb475cd6714035
Nicklas Körtge <Nicklas.Koertge1@ibm.com>
2025-10-21 16:57:52 +0200

    formamting
    Signed-off-by: Nicklas Körtge <Nicklas.Koertge1@ibm.com>

--- java/src/test/java/com/ibm/plugin/rules/issues/DuplicateParameterFindingsTest.java
+++ java/src/test/java/com/ibm/plugin/rules/issues/DuplicateParameterFindingsTest.java
@@ -19,6 +19,8 @@
  */
 package com.ibm.plugin.rules.issues;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.ibm.engine.detection.DetectionStore;
 import com.ibm.engine.model.IValue;
 import com.ibm.engine.model.ValueAction;
@@ -35,17 +37,14 @@ import com.ibm.mapper.model.PublicKeyEncryption;
 import com.ibm.mapper.model.functionality.Digest;
 import com.ibm.plugin.TestBase;
 import com.ibm.plugin.rules.detection.bc.BouncyCastleJars;
+import java.util.List;
+import javax.annotation.Nonnull;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 import org.sonar.plugins.java.api.JavaCheck;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
 import org.sonar.plugins.java.api.semantic.Symbol;
 import org.sonar.plugins.java.api.tree.Tree;
-
-import javax.annotation.Nonnull;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class DuplicateParameterFindingsTest extends TestBase {
 
